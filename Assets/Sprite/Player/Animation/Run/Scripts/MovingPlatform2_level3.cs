412af79d944bd1993f6429285ad160cff089da2e
sorana-beliat18 <sorana.beliat8@gmail.com>
2026-01-03 18:14:44 +0200

    Moving Platforms added

---
+++ Assets/Sprite/Player/Animation/Run/Scripts/MovingPlatform2_level3.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class MovingPlatform2_level3 : MonoBehaviour
+{
+    public float speed = 2f;
+    public float distance = 3f;
+    public bool moveHorizontal = true;
+    public bool invert = false;
+
+    private Vector3 startPos;
+
+    void Start()
+    {
+        // Salvăm poziția de unde pleacă platforma la începutul jocului
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        // Calculăm mișcarea stânga-dreapta ca un pendul
+        float movement = Mathf.PingPong(Time.time * speed, distance);
+
+        if(invert) movement=-movement;
+
+        if (moveHorizontal)
+            transform.position = startPos + new Vector3(movement, 0, 0);
+        else
+            transform.position = startPos + new Vector3(0, movement, 0);
+    }
+
+    // Această funcție face ca jucătorul să se miște ODATĂ cu platforma când stă pe ea
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Boss"))
+        {
+            collision.transform.SetParent(transform);
+        }
+    }
+
+    // Când jucătorul sare de pe platformă, nu mai este "purtat" de ea
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Boss"))
+        {
+            collision.transform.SetParent(null);
+        }
+    }
+}
---
+++ Assets/Sprite/Player/Animation/Run/Scripts/MovingPlatform4_level3.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class MovingPlatform4_level3 : MonoBehaviour
+{
+    public float speed = 2f;
+    public float distanceUp = 2f;    // Câte blocuri merge în sus
+    public float distanceDown = 3f;  // Câte blocuri merge în jos
+    public bool invert = false;      // Dacă vrei să înceapă mișcarea invers
+
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        // Calculăm o valoare care merge de la 0 la 1 și înapoi la 0
+        float pingPong = Mathf.PingPong(Time.time * speed, 1f);
+
+        // Transformăm acea valoare într-o mișcare între -distanceDown și +distanceUp
+        // Lerp face tranziția lină între cele două puncte
+        float yOffset = Mathf.Lerp(-distanceDown, distanceUp, pingPong);
+
+        if (invert) yOffset = -yOffset;
+
+        transform.position = startPos + new Vector3(0, yOffset, 0);
+    }
+
+    // Păstrăm logica pentru a purta Player-ul/Boss-ul
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Boss"))
+        {
+            collision.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Boss"))
+        {
+            collision.transform.SetParent(null);
+        }
+    }
+}
