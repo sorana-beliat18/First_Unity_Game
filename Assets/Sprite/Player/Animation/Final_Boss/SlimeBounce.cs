31ca4ef93c670f12799df09a363fe883f900108d
sorana-beliat18 <sorana.beliat8@gmail.com>
2026-01-08 23:38:19 +0200

    Slime bounce and sounds added

---
+++ Assets/Sprite/Player/Animation/Final_Boss/SlimeBounce.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class SlimeBounce : MonoBehaviour
+{
+    public float bounceForce = 15f; // Ajustează forța aici
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
+            rb.AddForce(Vector2.up * bounceForce, ForceMode2D.Impulse);
+
+            // --- ADAUGĂ ASTA ---
+            AudioSource audio = GetComponent<AudioSource>();
+            if (audio != null) audio.Play();
+            // -------------------
+        }
+    }
+}
