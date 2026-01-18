d447b2fb188afa9ee1ec5d17a32d600669ca509f
sorana-beliat18 <sorana.beliat8@gmail.com>
2026-01-18 15:06:12 +0200

    StartScreen complete

---
+++ Assets/Sprite/Player/Animation/Final_Boss/Final_Boss_scripts/StartScreenControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreenControl : MonoBehaviour
+{
+    // Această funcție va fi apelată de buton
+    public void StartGame()
+    {
+        // "Nivel1" trebuie să fie numele EXACT al scenei tale de joc
+        SceneManager.LoadScene("Level_1");
+    }
+
+    // Opțional: O funcție pentru a închide jocul
+    public void QuitGame()
+    {
+        Debug.Log("Jocul s-a închis!");
+        Application.Quit();
+    }
+}
